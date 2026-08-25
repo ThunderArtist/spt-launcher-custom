9d73f9576a3ba1a400519af0252bef9154e20c81
ThunderArtist <thunderartist@classdev.net>
2026-08-25 20:12:21 +0300

    Enhance automatic Proton Detection

--- SPTushonka.Core/Spt/Paths.cs
+++ SPTushonka.Core/Spt/Paths.cs
@@ -14,7 +14,16 @@ public class Paths
 
     public static readonly string[] ProtonPaths = {
         Path.Join(_userProfile, ".steam", "steam", "compatibilitytools.d"),
-        Path.Join("/", "usr", "share", "steam", "compatibilitytools.d")
+        Path.Join("/", "usr", "share", "steam", "compatibilitytools.d"),
+        Path.Join(_userProfile, ".local", "share", "Steam", "compatibilitytools.d"),
+        Path.Join(_userProfile, ".local", "share", "lutris", "runners", "proton"),
+        Path.Join(_userProfile, ".local", "share", "lutris", "runners", "wine"),
+        Path.Join(_userProfile, ".var", "app", "net.lutris.Lutris", "data", "lutris", "runners", "proton"),
+        Path.Join(_userProfile, ".var", "app", "net.lutris.Lutris", "data", "lutris", "runners", "wine"),
+        Path.Join(_userProfile, ".config", "heroic", "tools", "proton"),
+        Path.Join(_userProfile, ".config", "heroic", "tools", "wine"),
+        Path.Join(_userProfile, ".var", "app", "com.heroicgameslauncher.hgl", "config", "heroic", "tools", "proton"),
+        Path.Join(_userProfile, ".var", "app", "com.heroicgameslauncher.hgl", "config", "heroic", "tools", "wine"),
     };
 
     public static readonly string PatchPath = Path.Join(_runtimeRoot, "SPT_Data", "Launcher", "Patches");
