bb850f24eb84da76e73b6239264cf27195650b43
ThunderArtist <thunderartist@classdev.net>
2026-09-11 01:18:08 +0300

    Add Linux Clipboard Desktop Session Detection
    Needed to execute different commands for X11 and Wayland

--- SPTushonka.Core/Helpers/LinuxClipboard.cs
+++ SPTushonka.Core/Helpers/LinuxClipboard.cs
@@ -5,30 +5,55 @@ namespace SPTarkov.Core.Helpers;
 
 public static class LinuxClipboard
 {
-    // TODO: Add wayland support with wl-copy, the alternative of xclip for wayland
+    // Check whether the user is using X11 or Wayland
+    private static string GetSessionType()
+    {
+        var process = ExecuteCommand("echo $XDG_SESSION_TYPE");
+        return process.StandardOutput.ReadToEnd().Trim();
+    }
+    
     public static bool CopyText(string value)
     {
-        value = value.Replace("\"", "\\\"").Replace("\\", "\\\\\\");    // Escapes on top of escapes
-        var processInfo = new ProcessStartInfo("/usr/bin/sh", $"-c \"printf %b \\\"{value}\\\" | xclip -selection clipboard\"")
+        var sessionType = GetSessionType();
+
+        if (sessionType == "x11")
+            return CopyTextX11(value);
+        
+        if (sessionType == "wayland")
+            return CopyTextWayland(value);
+
+        throw new Exception($"Your window manager \"{sessionType}\" is not supported.");
+    }
+
+    public static bool CopyTextX11(string value)
+    {
+        value = value.Replace("\\", "\\\\").Replace("\"", "\\\"");      // Escapes on top of escapes
+        var process = ExecuteCommand($"printf %b \"{value}\" | xclip -selection clipboard");
+        return process.ExitCode == 0;
+    }
+
+    public static bool CopyTextWayland(string value)
+    {
+        // TODO: Add wayland support with wl-copy, the alternative of xclip for wayland
+        throw new NotImplementedException("Wayland clipboard is not supported yet. Make a pull request :)");
+    }
+
+    private static Process ExecuteCommand(string command)
+    {
+        command = command.Replace("\\", "\\\\").Replace("\"", "\\\"");    // Escapes on top of escapes
+        var processInfo = new ProcessStartInfo("/usr/bin/sh", $"-c \"{command}\"")
         {
             RedirectStandardOutput = true,
             UseShellExecute = false,
             CreateNoWindow = true,
         };
+    
+        Process? process = Process.Start(processInfo);
+        if (process == null)
+            throw new Exception("Process is null.");
 
-        Process? process = null;
-        try
-        {
-            process = Process.Start(processInfo);
-            process?.WaitForExit();
-        }
-        catch (Exception e)
-        {
-            // TODO: Logger!
-            Console.WriteLine(e.ToString());
-            process = null;
-        }
+        process.WaitForExit();
 
-        return process != null && process.ExitCode == 0;
+        return process;
     }
 }
